fa34db4610081fac0be8ec7837722a019b3c80f8
Shahil Gandhi <shahil_gandhi@vebuin.com>
2025-01-03 17:20:02 +0530

    - added pdf export from json

--- controllers/FormBuilderController.js
+++ controllers/FormBuilderController.js
@@ -402,57 +402,372 @@ app.controller('FormIOController', function (
             "display": "form",
             "components": [
                 {
-                    "label": "Text Field",
-                    "key": "textField",
-                    "type": "textfield",
-                    "input": true
+                  "title": "Basic",
+                  "collapsible": false,
+                  "key": "section",
+                  "type": "panel",
+                  "label": "Panel",
+                  "input": false,
+                  "tableView": false,
+                  "components": [
+                    {
+                      "label": "Name",
+                      "applyMaskOn": "change",
+                      "tableView": true,
+                      "validateWithKatakana": false,
+                      "validateWithHiragana": false,
+                      "validateWhenHidden": false,
+                      "key": "textField",
+                      "type": "textfield",
+                      "input": true
+                    },
+                    {
+                      "label": "Description",
+                      "applyMaskOn": "change",
+                      "autoExpand": false,
+                      "tableView": true,
+                      "validateWhenHidden": false,
+                      "key": "description",
+                      "type": "textarea",
+                      "input": true
+                    },
+                    {
+                      "label": "Date of Birth",
+                      "tableView": false,
+                      "datePicker": {
+                        "disableWeekends": false,
+                        "disableWeekdays": false
+                      },
+                      "enableMinDateInput": false,
+                      "enableMaxDateInput": false,
+                      "validateWhenHidden": false,
+                      "key": "dateOfBirth",
+                      "type": "datetime",
+                      "input": true,
+                      "widget": {
+                        "type": "calendar",
+                        "displayInTimezone": "viewer",
+                        "locale": "en",
+                        "useLocaleSettings": false,
+                        "allowInput": true,
+                        "mode": "single",
+                        "enableTime": true,
+                        "noCalendar": false,
+                        "format": "yyyy-MM-dd hh:mm a",
+                        "hourIncrement": 1,
+                        "minuteIncrement": 1,
+                        "time_24hr": false,
+                        "minDate": null,
+                        "disableWeekends": false,
+                        "disableWeekdays": false,
+                        "maxDate": null
+                      }
+                    },
+                    {
+                      "label": "Columns",
+                      "columns": [
+                        {
+                          "components": [
+                            {
+                              "label": "Slider Component",
+                              "tableView": false,
+                              "validateWhenHidden": false,
+                              "key": "slider",
+                              "type": "slider",
+                              "input": true
+                            }
+                          ],
+                          "width": 6,
+                          "offset": 0,
+                          "push": 0,
+                          "pull": 0,
+                          "size": "md",
+                          "currentWidth": 6
+                        },
+                        {
+                          "components": [
+                            {
+                              "label": "Hobbies",
+                              "optionsLabelPosition": "right",
+                              "tableView": false,
+                              "values": [
+                                {
+                                  "label": "Sports",
+                                  "value": "sports",
+                                  "shortcut": ""
+                                },
+                                {
+                                  "label": "Cooking",
+                                  "value": "cooking",
+                                  "shortcut": ""
+                                },
+                                {
+                                  "label": "Travelling",
+                                  "value": "travelling",
+                                  "shortcut": ""
+                                }
+                              ],
+                              "validateWhenHidden": false,
+                              "key": "hobbies",
+                              "type": "selectboxes",
+                              "input": true,
+                              "inputType": "checkbox"
+                            }
+                          ],
+                          "width": 6,
+                          "offset": 0,
+                          "push": 0,
+                          "pull": 0,
+                          "size": "md",
+                          "currentWidth": 6
+                        }
+                      ],
+                      "key": "columns",
+                      "type": "columns",
+                      "input": false,
+                      "tableView": false
+                    },
+                    {
+                      "label": "Columns",
+                      "columns": [
+                        {
+                          "components": [
+                            {
+                              "label": "English",
+                              "tableView": false,
+                              "validateWhenHidden": false,
+                              "key": "english",
+                              "type": "starrating",
+                              "input": true
+                            }
+                          ],
+                          "offset": 0,
+                          "push": 0,
+                          "pull": 0,
+                          "size": "md",
+                          "currentWidth": 4,
+                          "width": 4
+                        },
+                        {
+                          "components": [
+                            {
+                              "label": "Hindi",
+                              "tableView": false,
+                              "validateWhenHidden": false,
+                              "key": "hindi",
+                              "type": "starrating",
+                              "input": true
+                            }
+                          ],
+                          "offset": 0,
+                          "push": 0,
+                          "pull": 0,
+                          "size": "md",
+                          "currentWidth": 4,
+                          "width": 4
+                        },
+                        {
+                          "components": [
+                            {
+                              "label": "Gujarati",
+                              "tableView": false,
+                              "validateWhenHidden": false,
+                              "key": "gujarati",
+                              "type": "starrating",
+                              "input": true
+                            }
+                          ],
+                          "size": "md",
+                          "width": 4,
+                          "currentWidth": 4
+                        }
+                      ],
+                      "key": "columns1",
+                      "type": "columns",
+                      "input": false,
+                      "tableView": false
+                    },
+                    {
+                      "label": "NRI",
+                      "hideLabel": true,
+                      "tableView": false,
+                      "validateWhenHidden": false,
+                      "key": "nri",
+                      "type": "enabledisable",
+                      "input": true
+                    }
+                  ]
                 },
                 {
-                    "label": "Email",
-                    "key": "email",
-                    "type": "email",
-                    "input": true
+                  "title": "Education",
+                  "collapsible": false,
+                  "key": "education",
+                  "type": "panel",
+                  "label": "Panel",
+                  "input": false,
+                  "tableView": false,
+                  "components": [
+                    {
+                      "label": "Data Grid",
+                      "reorder": false,
+                      "addAnotherPosition": "bottom",
+                      "layoutFixed": false,
+                      "enableRowGroups": false,
+                      "initEmpty": false,
+                      "hideLabel": true,
+                      "tableView": false,
+                      "defaultValue": [
+                        {}
+                      ],
+                      "validateWhenHidden": false,
+                      "key": "dataGrid",
+                      "type": "datagrid",
+                      "input": true,
+                      "components": [
+                        {
+                          "label": "Degree",
+                          "applyMaskOn": "change",
+                          "tableView": true,
+                          "validateWithKatakana": false,
+                          "validateWithHiragana": false,
+                          "validateWhenHidden": false,
+                          "key": "degree",
+                          "type": "textfield",
+                          "input": true
+                        },
+                        {
+                          "label": "University",
+                          "applyMaskOn": "change",
+                          "tableView": true,
+                          "validateWithKatakana": false,
+                          "validateWithHiragana": false,
+                          "validateWhenHidden": false,
+                          "key": "university",
+                          "type": "textfield",
+                          "input": true
+                        },
+                        {
+                          "label": "Percentage/CGPA",
+                          "applyMaskOn": "change",
+                          "tableView": true,
+                          "validateWithKatakana": false,
+                          "validateWithHiragana": false,
+                          "validateWhenHidden": false,
+                          "key": "percentageCgpa",
+                          "type": "textfield",
+                          "input": true
+                        }
+                      ]
+                    }
+                  ]
                 },
                 {
-                    "label": "Date",
-                    "key": "date",
-                    "type": "datetime",
-                    "input": true
+                  "title": "Resume Upload",
+                  "collapsible": false,
+                  "key": "resumeUpload",
+                  "type": "panel",
+                  "label": "Panel",
+                  "input": false,
+                  "tableView": false,
+                  "components": [
+                    {
+                      "label": "Upload",
+                      "tableView": false,
+                      "storage": "base64",
+                      "image": true,
+                      "webcam": false,
+                      "fileTypes": [
+                        {
+                          "label": "",
+                          "value": ""
+                        }
+                      ],
+                      "validateWhenHidden": false,
+                      "key": "file",
+                      "type": "file",
+                      "input": true
+                    }
+                  ]
                 },
                 {
-                    "label": "Submit",
-                    "key": "submit",
-                    "type": "button",
-                    "input": true
+                  "title": "Signature",
+                  "collapsible": false,
+                  "key": "signature",
+                  "type": "panel",
+                  "label": "Panel",
+                  "input": false,
+                  "tableView": false,
+                  "components": [
+                    {
+                      "label": "Signature",
+                      "tableView": false,
+                      "validateWhenHidden": false,
+                      "key": "signature1",
+                      "type": "signature",
+                      "input": true
+                    }
+                  ]
                 }
-            ]
+              ]
         };
 
         const pdf = new jsPDF(); // Create a new jsPDF instance
         let yPosition = 10; // Initial Y position for content
 
-        // Add Form Title
-        pdf.setFontSize(16);
-        pdf.text("Form Export", 10, yPosition);
-        yPosition += 10;
-
         // Iterate Over Components in JSON
-        formJson.components.forEach((component) => {
+        [formJson.components[0]].forEach((component) => {
             if (yPosition > 280) { // Assuming A4 page height is 297mm
                 pdf.addPage();
                 yPosition = 10; // Reset Y position for the new page
             }
+            let position;
             if (component.type !== 'button') {
-                // Add Label
-                pdf.setFontSize(12);
-                pdf.text(component.label, 10, yPosition);
-                yPosition += 2;
+                if(component.type === 'panel'){
+                  position = yPosition;
+                  component.components.forEach(element => {
+                    if(element.type !== 'columns'){
+                      position += 10;
+                      pdf.setFontSize(12);
+                      pdf.text(element.label, 7, position + 6);
+                      position += 10;
+                      pdf.setDrawColor(0); // Black border
+                      pdf.setLineWidth(0.1);
+                      pdf.rect(7, position, 190, 10)
+                    }
+                    if(element.type === 'columns'){
+                      const columnWidth = Math.round(200 / element.columns.length)
+                      console.log("columnWidth", columnWidth);
 
-                // Add Placeholder for Field
+                        let xIndex = 7;
+                        position += 10;
+                      element.columns.forEach((column, index) => {
+                        if(index > 0){
+                          xIndex += columnWidth
+                        }
+                        column.components.forEach((item) => {
+                          
+                          if(item.type === 'starrating'){
+                            
+                            pdf.setFontSize(12);
+                            pdf.text(`${item.label}: 4/5`, xIndex, position);
+                          }
+                        })
+                      })
+                    }
+                  })
+                }
+                
                 pdf.setDrawColor(0); // Black border
                 pdf.setLineWidth(0.1);
-                pdf.rect(10, yPosition, 100, 7); // Placeholder box
-                yPosition += 15;
+                pdf.setFillColor(211,211,211);
+                pdf.rect(5, yPosition, 200, 10, 'F')
+                pdf.setFontSize(12);
+                pdf.text(component.title, 7, yPosition + 6);
+                pdf.setDrawColor(0); // Black border
+                pdf.setLineWidth(0.1);
+                pdf.rect(5, yPosition, 200, position + 15)
+                
+                // yPosition += 105;
             }
         });
 
@@ -480,7 +795,7 @@ app.controller('FormIOController', function (
         // Add a listener to Dragula instance
         builderInstance.on('addComponent', function(component) {
             const element = document.getElementById(component.id)
-            
+            console.log("element", element);
             // Validate if the component is dropped inside a valid panel
             const isValidZone = element && element.closest('.formio-component-panel');
             if (!isValidZone) {
